bf51f63c60230f7ea5721d2813537bdfabe2d4ff
Damian Lammenda <damianlammenda@hotmail.com>
2023-06-25 13:30:44 -0300

    TP002

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
-import ProductManager from './productManager.js';
-
+import ProductManager from "./productManager.js";
 
 // Ejemplo de uso
-const productManager = new ProductManager();
+const productManager = new ProductManager("products.json");
 
 productManager.addProduct({
-  title: 'Remera',
-  description: 'Remera de algodón',
+  title: "Remera",
+  description: "Remera de algodón",
   price: 20,
-  thumbnail: './img/cam001.jpg',
-  code: 'CAM001',
+  thumbnail: "./img/cam001.jpg",
+  code: "CAM001",
   stock: 10,
 });
 
 productManager.addProduct({
-    title: 'Pantalón',
-    description: 'Pantalón de jean',
-    price: 50,
-    thumbnail: './img/p001.jpeg',
-    code: 'PAN001',
-    stock: 10,
+  title: "Pantalón",
+  description: "Pantalón de jean",
+  price: 50,
+  thumbnail: "./img/p001.jpeg",
+  code: "PAN001",
+  stock: 10,
 });
 
+// const allProducts = productManager.getProducts();
+// console.log(`All products:`, allProducts);
 
+// const productById = productManager.getProductById(allProducts[0].id);
+// console.log(`Product by id:`, productById);
 
-const allProducts = productManager.getProducts();
-console.log(`All products:`, allProducts);
+productManager.updateProduct(
+  "5710b967-f09b-4ead-a115-27246c769cad",
+  { title: "Remera actualizada", price: 390 }
+);
 
-const productById = productManager.getProductById(allProducts[0].id);
-console.log(`Product by id:`, productById);
+//productManager.deleteProduct("5710b967-f09b-4ead-a115-27246c769cad");
--- src/productManager.js
+++ src/productManager.js
@@ -1,29 +1,92 @@
-import { v4 as uuidv4 } from 'uuid';
+import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
 
-export default class ProductManager {
-  constructor() {
+class ProductManager {
+  constructor(path) {
     this.products = [];
-    this.productId = 0;
+    this.path = path;
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.path, "utf8");
+      this.products = JSON.parse(data);
+    } catch (error) {
+      // Si ocurre un error al leer el archivo, se asume que el archivo no existe o está vacío.
+      // Se inicializa la lista de productos como un arreglo vacío.
+      this.products = [];
+    }
+  }
+
+  saveProducts() {
+    const data = JSON.stringify(this.products, null, 2);
+    fs.writeFileSync(this.path, data);
   }
 
   addProduct(product) {
     if (!this.isProductValid(product)) {
-      console.error('All fields are required.');
+      console.log("Error: Todos los campos del producto son obligatorios.");
       return;
     }
 
-    if (this.isCodeDuplicate(product.code)) {
-      console.error('The code is already in use.');
+    const isCodeDuplicate = this.products.some((p) => p.code === product.code);
+    if (isCodeDuplicate) {
+      console.log("Error: El código del producto ya existe.");
       return;
     }
 
     const newProduct = {
       ...product,
-        id: uuidv4(),
+      id: uuidv4(),
     };
 
     this.products.push(newProduct);
-    console.info('Product added:', newProduct);
+    this.saveProducts(); // Guardar los productos actualizados en el archivo
+    console.log("Producto agregado:", newProduct);
+  }
+
+  updateProduct(id, updatedFields) {
+    const productIndex = this.products.findIndex((p) => p.id === id);
+
+    if (productIndex === -1) {
+      console.log("Error: Producto no encontrado.");
+      return;
+    }
+
+    const updatedProduct = {
+      ...this.products[productIndex],
+      ...updatedFields,
+      id,
+    };
+
+    // Validar si el código del producto actualizado ya existe, excluyendo el producto actual
+    const isCodeDuplicate = this.products.some(
+      (p) => p.code === updatedProduct.code && p.id !== id
+    );
+
+    if (isCodeDuplicate) {
+      console.log("Error: El código del producto ya existe.");
+      return;
+    }
+
+    this.products[productIndex] = updatedProduct;
+    this.saveProducts(); // Guardar los productos actualizados en el archivo
+    console.log("Producto actualizado:", updatedProduct);
+    return updatedProduct; // Devolver el producto actualizado
+  }
+
+  deleteProduct(id) {
+    const productIndex = this.products.findIndex((p) => p.id === id);
+
+    if (productIndex === -1) {
+      console.log("Error: Producto no encontrado.");
+      return;
+    }
+
+    const deletedProduct = this.products.splice(productIndex, 1)[0];
+    this.saveProducts(); // Guardar los productos actualizados en el archivo
+    console.log("Producto eliminado:", deletedProduct);
   }
 
   getProducts() {
@@ -35,18 +98,73 @@ export default class ProductManager {
     if (product) {
       return product;
     } else {
-      console.error('Product not found.');
+      console.log("Error: Producto no encontrado.");
+      return undefined;
     }
   }
 
   isProductValid(product) {
     const { title, description, price, thumbnail, code, stock } = product;
-    return title && description && price && thumbnail && code && stock;
-  }
-
-  isCodeDuplicate(code) {
-    return this.products.some((product) => product.code === code);
+    return (
+      title !== undefined &&
+      description !== undefined &&
+      price !== undefined &&
+      thumbnail !== undefined &&
+      code !== undefined &&
+      stock !== undefined
+    );
   }
 }
 
+export default ProductManager;
 
+// import { v4 as uuidv4 } from 'uuid';
+
+// export default class ProductManager {
+//   constructor() {
+//     this.products = [];
+//     this.productId = 0;
+//   }
+
+//   addProduct(product) {
+//     if (!this.isProductValid(product)) {
+//       console.error('All fields are required.');
+//       return;
+//     }
+
+//     if (this.isCodeDuplicate(product.code)) {
+//       console.error('The code is already in use.');
+//       return;
+//     }
+
+//     const newProduct = {
+//       ...product,
+//         id: uuidv4(),
+//     };
+
+//     this.products.push(newProduct);
+//     console.info('Product added:', newProduct);
+//   }
+
+//   getProducts() {
+//     return this.products;
+//   }
+
+//   getProductById(id) {
+//     const product = this.products.find((p) => p.id === id);
+//     if (product) {
+//       return product;
+//     } else {
+//       console.error('Product not found.');
+//     }
+//   }
+
+//   isProductValid(product) {
+//     const { title, description, price, thumbnail, code, stock } = product;
+//     return title && description && price && thumbnail && code && stock;
+//   }
+
+//   isCodeDuplicate(code) {
+//     return this.products.some((product) => product.code === code);
+//   }
+// }
