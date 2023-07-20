67a4ad139162487206483685a018e45be09ec24c
Damian Lammenda <damianlammenda@hotmail.com>
2023-07-20 11:06:09 -0300

    handlebars

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from "express";
 import morgan from "morgan";
 import productRouter from "./src/router/products.routes.js";
 import cartRouter from "./src/router/cart.routes.js";
+import viewRouter from "./src/router/views.routes.js";
+import realTimeProducts from "./src/router/realTimeProducts.routes.js";
+import __dirname from "./utils.js";
+import { engine } from "express-handlebars";
+
 
 
 const app = express();
@@ -9,10 +14,14 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./src/views"));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.use(express.static("./public"));
+app.set("views", `${__dirname}/views`)
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
-
+app.use("/realtimeproducts", realTimeProducts);
+app.use("/", viewRouter);
 
 
 export default app;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import app from "./app.js";
 import { config } from "dotenv";
 config();
+import { Server } from "socket.io";
 
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    }   
-);
+
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export const io = new Server(httpServer);
